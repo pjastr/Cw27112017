c773b0e96f9af076e56b86f1b3ed7f6184fc86ae
pjastr <24464854+pjastr@users.noreply.github.com>
2017-11-27 12:55:35 +0100

    Add files via upload

---
+++ ConsoleApplication55/ConsoleApplication55/Urodziny.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication55
+{
+    class Urodziny
+    {
+        private string miejsceUr; //deklaracja pola
+        public Urodziny() { } //konstruktor domyślny
+
+        //konstruktor parametryczny
+        public Urodziny(string miejsceUr)
+        {
+            this.miejsceUr = miejsceUr;
+        }
+
+        //metoda na ustawienie miejsca urodzin
+        public void UstawMiejsceUr(string miejsceUr)
+        {
+            this.miejsceUr = miejsceUr;
+        }
+
+        //metoda na pobranie miejsca urodzenia
+        public string PobierzMiejsceUr()
+        {
+            return miejsceUr;
+        }
+    }
+}
---
+++ ConsoleApplication57/ConsoleApplication57/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConsoleApplication57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj date w formacie Thu 16 Nov 8:30 AM 2017");
+            string wejscie = Console.ReadLine();
+            string format = "ddd dd MMM h:mm tt yyyy";
+            DateTime dateTime;
+            DateTime.TryParseExact(wejscie, format, CultureInfo.InvariantCulture,
+            DateTimeStyles.None, out dateTime);
+
+            Console.WriteLine(dateTime);
+            Console.ReadKey();
+        }
+    }
+}
